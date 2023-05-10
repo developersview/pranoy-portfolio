68cd5d066ea2a8d041744d7a65bcaeef9652a963
developersview <pcslg1998@gmail.com>
2023-05-10 21:43:00 +0530

    nav, about, experience

--- pranoy-portfolio-website/src/components/about/About.jsx
+++ pranoy-portfolio-website/src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './About.css';
+import ME from '../../assets/me-about.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
     return (
-        <section id="about">About</section>
+        <section id="about">
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt='About Image' />
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <div className="about__card">
+                            <FaAward className='about__icon' />
+                            <h5>Skills</h5>
+                            <small>3+ Years Working</small>
+                        </div>
+                        <div className="about__card">
+                            <FiUsers className='about__icon' />
+                            <h5>Company</h5>
+                            <small>Software Engineer at CGI</small>
+                        </div>
+                        <div className="about__card">
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>Multiple technologies and Fullstack</small>
+                        </div>
+                    </div>
+                    <p>
+                        I have 2.9 Years of IT experience in analysis, design, documentation, Enhancement and development of applications in Java and Microsoft Azure, DevOps. Having knowledge with advanced technologies such as Java, Spring Boot, SQL Server, Azure DevOps, Logic Apps, Power Automate and Frontend Tools, React. Have a good communication, presentation, analytical, client facing and team management skills.
+                    </p>
+                    <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- pranoy-portfolio-website/src/components/experience/Experience.jsx
+++ pranoy-portfolio-website/src/components/experience/Experience.jsx
@@ -1,9 +1,112 @@
 import React from 'react'
 import './Experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
     return (
-        <section id='experience'>Skill Set</section>
+        <section id='experience'>
+            <h5>What Skills I Have</h5>
+            <h2>My Experience</h2>
+            <div className="container experience_container">
+
+                <div className="experience__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>HTML</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>CSS</h4>
+                                <small className='text-light'>Intermidiate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Bootstarp</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intermidiate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>JavaScript</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Tailwind</h4>
+                                <small className='text-light'>Beginner</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                <div className="experience__backend">
+                    <h3>Backend Development</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>JAVA</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Node.js</h4>
+                                <small className='text-light'>Intermidiate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Spring Boot</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>SQL Server, MongoDB</h4>
+                                <small className='text-light'>Intermidiate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Logic Apps, Git</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Python</h4>
+                                <small className='text-light'>Beginner</small>
+                            </div>
+                        </article>
+                    </div>
+
+                </div>
+            </div>
+        </section>
     )
 }
 
