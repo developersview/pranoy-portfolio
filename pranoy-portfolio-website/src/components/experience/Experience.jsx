e0ee95ef65d17fdc634583607c43451be206395e
developersview <pcslg1998@gmail.com>
2023-05-10 19:25:15 +0530

    1st part done with initial commit

---
+++ pranoy-portfolio-website/src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Experience.css'
+
+const Experience = () => {
+    return (
+        <section id='experience'>Skill Set</section>
+    )
+}
+
+export default Experience
